2e15dd53d6706e7066f5805cef5fa8d61f9f2e8a
daniel <daniel.heineras@gmail.com>
2022-09-02 19:54:57 +0200

    Updates

--- client/src/service/service.js
+++
@@ -1,6 +0,0 @@
-import http from '../http-common'
-
-//* POST 
-export const contactPost = (data) => {return http.post("/contactpost",data)}
-export const careerPost = (data) => {return http.post("/careerpost",data)}
-export const servicePost = (data) => {return http.post("/servicepost",data)}
